fe34b691dc33a1966d607bc5afed884eccb0ec8a
esoterik <esoterik_xiao@askey.com>
2023-07-06 18:03:41 +0800

    update ui

--- app/src/main/java/com/soul/mvvmbase/ui/fragment/CurrentWeatherFragment.kt
+++ app/src/main/java/com/soul/mvvmbase/ui/fragment/CurrentWeatherFragment.kt
@@ -1,7 +1,6 @@
 package com.soul.mvvmbase.ui.fragment
 
-import android.content.pm.PackageInfo
-import android.content.pm.PackageManager
+import android.graphics.drawable.PictureDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,26 +8,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
-import com.afollestad.assent.Permission
-import com.afollestad.assent.runWithPermissions
-
-import com.amap.api.location.AMapLocationClient
 import com.amap.api.location.AMapLocationClientOption
-import com.amap.api.location.AMapLocationListener
-import com.soul.mvvmbase.data.bean.WeatherLocation
+import com.caverock.androidsvg.SVG
 import com.soul.mvvmbase.data.viewmodel.CurrentWeatherViewModel
 import com.soul.mvvmbase.databinding.FragmentCurrentWeatherBinding
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.math.BigDecimal
-import java.security.MessageDigest
+import java.io.IOException
+import java.io.InputStream
 import java.util.*
 
 
@@ -73,6 +62,11 @@ class CurrentWeatherFragment : Fragment() {
                 currentWeatherBinding.groupLoading.visibility = View.GONE
 //                    currentWeatherBinding.textview.text = it.toString()
                 currentWeatherBinding.textview.visibility = View.GONE
+                updateIcon(it.icon)
+                updateWeatherText(it.text)
+                updateWeatherWindInfo(it.windDir,it.windScale)
+                updateHumidity(it.humidity)
+                updateVis(it.vis)
             })
         }
     }
@@ -94,6 +88,52 @@ class CurrentWeatherFragment : Fragment() {
     private fun updateTemperatures(tmp: String, feelsTmp: String) {
         currentWeatherBinding.textviewTmp.text = "$tmp°C"
         currentWeatherBinding.textviewFeelsLikeTmp.text = "Feels like $feelsTmp°C"
+    }
+    private fun updateWeatherText(text: String) {
+        currentWeatherBinding.weatherText.text = text
+    }
+    private fun updateWeatherWindInfo(windDir: String,windScale :String) {
+        currentWeatherBinding.textviewWindDir.text = "风向:$windDir"
+        var tmp = windScale.toInt()
+        var scale = when(tmp){
+            0 -> "无风"
+            1 -> "软风"
+            2 -> "轻风"
+            3 -> "微风"
+            4 -> "和风"
+            5 -> "清风"
+            6 -> "强风"
+            7 -> "疾风"
+            8 -> "大风"
+            9 -> "烈风"
+            10 -> "狂风"
+            11 -> "暴风"
+            12 -> "飓风"
+            else -> "台风"
+        }
+        currentWeatherBinding.textviewWindScale.text = "风力:$scale"
+    }
+    private fun updateHumidity(humidity : String) {
+        currentWeatherBinding.textviewHumidity.text = "相对湿度:$humidity%"
+    }
+    private fun updateVis(vis : String) {
+        currentWeatherBinding.textviewVis.text = "可见度:${vis}公里"
+    }
+    private fun updateIcon(img:String){
+        try {
+            // 从Assets目录下读取SVG文件
+            val inputStream: InputStream = requireContext().assets.open("${img}.svg")
+            // 使用AndroidSVG库解析SVG文件
+            val svg = SVG.getFromInputStream(inputStream)
+            // 创建一个PictureDrawable对象
+            val pictureDrawable = PictureDrawable(svg.renderToPicture())
+            // 将PictureDrawable对象设置给ImageView
+            currentWeatherBinding.imageViewWeather.setImageDrawable(pictureDrawable)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+
     }
 
 }
